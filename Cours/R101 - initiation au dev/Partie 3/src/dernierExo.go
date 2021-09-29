69ede45052c612becfb298114e43fe0796d3a8bc
LLomann <lomannlcq@hotmail.fr>
2021-09-29 10:42:47 +0200

    exo de dev partie3

--- Cours/R101 - initiation au dev/Partie 3/src/dernierExo.go
+++ Cours/R101 - initiation au dev/Partie 3/src/dernierExo.go
@@ -15,8 +15,15 @@ func main(){
 	var filePath string = "notes.csv"
 	var myFile *os.File
 	var err error
-	var ligne int = 1
-
+	var compte int = 0
+	var ligne int = 2
+	var note int
+	var bonus int
+	var total int
+	var nom string = "Jean-François"
+	var somme int =0
+	var nb int = 0
+	
 	myFile, err = os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -27,13 +34,30 @@ func main(){
 	scanner = bufio.NewScanner(myFile)
 
 
-	for scanner.Scan(){
+	scanner.Scan() // lis 1er ligne
+	
+	
+	for scanner.Scan(){ // lis a partir de le seconde ligne jusqu'à la fin
 		var tab []string = strings.Split(scanner.Text(),",")
-		
-		if tab[1]int + tab[2]int != tab[3]int{
-			ligne += 1
-			tab[] 
-			fmt.Println("il y a une faute a la ligne:"ligne)
+		note,err = strconv.Atoi(tab[1])
+		bonus,err= strconv.Atoi(tab[2])
+		total,err = strconv.Atoi(tab[3])
+
+		if err != nil{
+			fmt.Println("la note n'est pas écrit en chiffre à la ligne", ligne)
 		}
+
+		if note+bonus != total{ 
+			fmt.Println("il y a une faute a la ligne:", ligne)
+			compte += 1
+		}
+
+		if tab[0] == nom{
+			somme = total + somme
+			nb += 1
+		}
+		ligne += 1
 	}
+	fmt.Println("la moyenne de",nom,"est de", float64(somme)/float64(nb))
+	fmt.Println("Il y a en tous", compte,"fautes")
 }
