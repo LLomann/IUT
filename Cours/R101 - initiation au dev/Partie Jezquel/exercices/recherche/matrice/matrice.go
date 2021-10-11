d4de66efa342de094279ad53c171cd29cf2116ea
LLomann <lomannlcq@hotmail.fr>
2021-10-11 17:46:50 +0200

    dev

--- Cours/R101 - initiation au dev/Partie Jezquel/exercices/recherche/matrice/matrice.go
+++ Cours/R101 - initiation au dev/Partie Jezquel/exercices/recherche/matrice/matrice.go
@@ -22,5 +22,27 @@ la même longueur ou mat vaut nil ou une ligne vaut nil)
 compte(0, [][]int{[]int{0, 1}, []int{0, 0}}) = 3
 */
 func compte(n int, mat [][]int) (num int, err error) {
-	return num, err
+
+	if mat == nil {
+		return 0, errPasMat
+	}
+
+	if len(mat) == 0 {
+		return 0, nil
+	}
+
+	var res int
+	var longueur int = len(mat[0])
+	for _, tab := range mat { //	fo i:=0; i<len(mat); i++ { 	//	tab = mat[i]
+		if len(tab) != longueur || tab == nil {
+			return 0, errPasMat
+		}
+		for _, el := range tab { //	fo j:=0; len(tab); j++ 	//		el = tab[j]
+			if el == n {
+				res += 1
+			}
+		}
+	}
+
+	return res, nil
 }
--- Cours/R101 - initiation au dev/Partie Jezquel/exercices/recherche/maxoccurrences/maxoccurrences.go
+++ Cours/R101 - initiation au dev/Partie Jezquel/exercices/recherche/maxoccurrences/maxoccurrences.go
@@ -17,5 +17,27 @@ entier apparaît. En cas d'égalité on choisira arbitrairement l'un des entiers
 maxoccurrences([]int{1, 2, 3, 4, 3}) = 3, 2
 */
 func maxoccurrences(t []int) (n int, occ int) {
+
+	if t == nil {
+		return 69,0
+	}
+
+	var compteur int
+	var ancien_compteur int
+	for i:=0; i<len(t); i ++ {
+		for j:= i+1;j<len(t); j ++ {
+			if t[i] == t[j]{
+				compteur ++
+			}
+		}
+	
+	if ancien_compteur > compteur {
+		ancien_compteur = compteur
+		n = t[i]
+		occ = compteur
+	}
+	compteur = 0	
+	} 
+
 	return n, occ
 }
