9342122bd4d98385601b346cac3480ecdf13e630
LLomann <lomannlcq@hotmail.fr>
2021-10-20 19:44:22 +0200

    +

--- Cours/R101 - initiation au dev/ESSAIE.go
+++ Cours/R101 - initiation au dev/ESSAIE.go
@@ -1,25 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"log"
-)
-func main() {
-	var myFile *os.File
-	var err error
-	myFile, err = os.Create("Ouch")
-	if err != nil{
-		log.Fatal(err)
-	}
+import "fmt"
 
-	_, err = fmt.Fprintln(myFile, "Ouch parce que t nul")
-	if err != nil{
-		log.Fatal(err)
-	}
+func table_de(x *int, y *int){
 
-	err = myFile.Close()
-	if err != nil{
-		log.Fatal(err)
+	for i:=0; i < y; i++{
+		fmt.Println(i, "x", x, "=", i*x)
 	}
+}
+
+func main(){
+
+	table_de(5,10)
+	
 }
